28d5a92d88f745d99524159c29e5ea57358d556f
Hubert-Zhu <zhubolin010118@gmail.com>
2020-08-16 23:50:45 -0400

    Half done the about page(too tried)

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -16,66 +16,95 @@ const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <div>
     <Nav />
 
-    <div className="intor__div">
-      <img src={Avatar} alt="touxiang" />
+    <div className="intro">
+      <img className="intro__avatar" src={Avatar} alt="touxiang" />
       <div>
-        <h1>
+        <h1 className="intro__name">
           Hey! I am <span>Hubert Zhu!</span>
         </h1>
         <ul>
-          <li>🎉🎉🎉Welcome to my website🎉🎉🎉</li>
-          <li>💻 I’m a Web Frontend Developer </li>
-          <li>
+          <li className="intro__self-intro">🎉 Welcome to my website</li>
+          <li className="intro__self-intro">
+            💻 I’m a Web Frontend Developer{" "}
+          </li>
+          <li className="intro__self-intro">
             🎓 I’m a Computer Engineering Undergraduate at the University of
             Waterloo
           </li>
-          <li>
+          <li className="intro__self-intro">
             😂 I’m currently learning Vue, TypeScript, etc... (a lot to learn)
           </li>
-          <li>💬 Oh! I'm looking for Co-op job for Winter 2020.</li>
-          <li>
-            💬 Please contact me if you offer any Front-end or Full-Stack
+          <li className="intro__self-intro">
+            💬 Oh! I'm looking for Co-op job for Winter 2020.
+          </li>
+          <li className="intro__self-intro">
+            💼 Please contact me if you offer any Front-end or Full-Stack
             position{" "}
           </li>
         </ul>
       </div>
     </div>
 
-    <div className="hr__div--gray"></div>
+    <div className="hr--gray"></div>
 
-    <div className="toolbox__div">
-      <h2>My Toolbox</h2>
+    <div className="tool-box">
+      <h2 className="tool-box__title">My Toolbox</h2>
 
-      <div className="toolBoxUnit__div">
-        <p className="topic__p--bold">Languages:</p>
-        <p>JavaScript (ES6, NodeJS), TypeScript, HTML5/CSS3, SASS/LESS, C++</p>
-      </div>
-
-      <div className="toolBoxUnit__div">
-        <p className="topic__p--bold"> Framework/Library:</p>
-        <p>
-          React, Gatsby, Vue, Bootstrap, jQuery, Express.js, Next.js, Lodash
+      <div className="tool-box__unit">
+        <p className="tool-box__unit-topic">Languages:</p>
+        <p className="tool-box__items">
+          JavaScript (ES6, NodeJS), TypeScript, HTML5, CSS3, SASS/LESS, C++
         </p>
       </div>
 
-      <div className="toolBoxUnit__div">
-        <p className="topic__p--bold"> Design Tools</p>{" "}
-        <p>Adobe XD, InVision, Figma</p>
+      <div className="tool-box__unit">
+        <p className="tool-box__unit-topic">Framework/Library:</p>
+        <p className="tool-box__items">
+          React, Gatsby, Next, Vue, Bootstrap, jQuery, Express,
+        </p>
       </div>
-      <div className="toolBoxUnit__div">
-        <p className="topic__p--bold"> Other Technologies: </p>
-        <p>RESTful, GraphQL, Ajax, Axios, SSR, Data Visualization, HTTP/TCP</p>
+
+      <div className="tool-box__unit">
+        <p className="tool-box__unit-topic">Technologies: </p>
+        <p className="tool-box__items">
+          RESTful API, GraphQL API, Ajax, Axios, SSR, Data Visualization,
+          HTTP/TCP
+        </p>
       </div>
-      <div className="toolBoxUnit__div">
-        <p className="topic__p--bold">Other Tools</p>
-        <p>
-          Git/GitHub, Styled-Components, Redux, MobX, Vuex, Gulp, Grunt,
-          Storybook, Postman, Webpack, Rollup, Parcel, Jest, Enzyme, Netlify,
-          Apache Echarts, MySQL Workbench, Chrome Devtools, NPM/Yarn, Bootstrap,
-          Google Firebase
+
+      <div className="tool-box__unit">
+        <p className="tool-box__unit-topic">Database: </p>
+        <p className="tool-box__items">MongoDB, MySQL(MySQL Workbench)</p>
+      </div>
+
+      <div className="tool-box__unit">
+        <p className="tool-box__unit-topic">Design Tools:</p>
+        <p className="tool-box__items">Adobe XD, InVision, Figma</p>
+      </div>
+
+      <div className="tool-box__unit">
+        <p className="tool-box__unit-topic">Other Tools:</p>
+        <p className="tool-box__items">
+          Git/GitHub, Styled-Component, Redux, Vuex, Gulp, Grunt, Storybook,
+          Postman, Webpack, Rollup, Parcel, Jest, Enzyme, Netlify, Apache
+          Echarts, Google Firebase
         </p>
       </div>
     </div>
+
+    <div className="hr--gray"></div>
+
+
+    <div className="contact">
+      <h2 className="contact__title" >Contact Me </h2>
+      
+      <div className="contact__item" >Linkdin: https://www.linkedin.com/in/hubert-zhu-23002b19b/</div>
+
+      <div className="contact__item" >Email: zhubolin010118@gmail.com</div>
+      
+      <div className="contact__item" >Github: https://github.com/Hubert-Zhu</div>
+    </div>
+    
   </div>
 )
 
