0d345f8fd939c041c1e4a05d7fbac04466ba6e75
Hubert-Zhu <zhubolin010118@gmail.com>
2020-08-24 16:47:25 -0400

    Blogs

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,14 +1,27 @@
 import React from "react"
-import { PageProps, graphql } from "gatsby"
+import { graphql, withPrefix, useStaticQuery } from "gatsby"
+import blogIcon from "./blog.svg"
+import "./style.scss"
+import Footer from "../../components/Footer"
+import Navbar from "../../components/Navbar"
 
-const project = () => (
-  <>
+const Blogs = React.memo(() => {
+  return (
     <div>
-       Hello World
+      <Navbar />
+      <div className={"blogs"}>
+        <h1 className={"blogs__title"}>Personal Blogs</h1>
+        <div className={"blogs__icon-container"}>
+          <div className="blogs__line"></div>
+          <div>
+            <img className="blogs__icon" src={blogIcon} alt="icons" />
+          </div>
+          <div className="blogs__line"></div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  </>
-)
+  )
+})
 
-export default project
-
-
+export default Blogs
