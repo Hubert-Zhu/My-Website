5a044402524a604841b67d14fc7386b5910ac467
Hubert-Zhu <zhubolin010118@gmail.com>
2020-08-28 11:17:10 -0400

    fix: typescript image import

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -7,7 +7,7 @@ import Footer from "../../components/Footer"
 import Project from "../../images/projects/project.svg"
 import project1 from "../../images/projects/mini-ui-library/project1.jpg"
 import project2 from "../../images/projects/my-blog/project2.png"
-import project3 from "../../images/projects/project3.jpg"
+import project3 from "../../images/projects/go-chat/project3.jpg"
 
 import "./style.scss"
 
